1d82e48c728b6344d4225ff0df1ae6613b104e89
iliapnmrv <iliapnmrvv@gmail.com>
2022-06-14 11:30:07 +0300

    gitignore cache fix

--- client/lib/apollo.ts
+++
@@ -1,9 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const apolloClient = new ApolloClient({
-  uri: "http://localhost:8002/graphql/api",
-  // uri: "http://mo4-it5:8002/graphql/api",
-  cache: new InMemoryCache(),
-});
-
-export default apolloClient;
